0d30fbc9dd54d9df7a0b3e8bf6b23f0cdb7ceadf
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-02-15 20:17:48 -0300

    Utilização de static.
    Uso do static para calcular o total de contas que foram abertas, sempre falando com a classe Conta e não mais com os atributos da conta que foi aberta.

--- ByteBankEncapsulado/TestaValores.java
+++ ByteBankEncapsulado/TestaValores.java
@@ -2,10 +2,13 @@ package ByteBankEncapsulado;
 
 public class TestaValores {
     public static void main(String[] args) {
-        Conta conta = new Conta(-1, 1500);
-
-        //conta está inconsistente a regra que foi criada.
+        Conta conta = new Conta(1, 1500);
         System.out.println(conta.getAgencia());
+
+        Conta conta2 = new Conta(12, 15004);
+        System.out.println(conta2.getAgencia());
+        
+        System.out.println(Conta.getTotal());
         
     }
 }
