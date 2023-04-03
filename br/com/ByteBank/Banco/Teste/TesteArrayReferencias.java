eb4289a29f13f1ffb9529d1b4e29ab5bf192d3b0
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-04-03 19:59:21 -0300

    Array com referencias
    As referencias, seja ela da mais genérica até a mais especifica cria array onde podemos ir adicionando objetos. Utilizando type cast para tornar a referencia genérica a mais especifica.

--- br/com/ByteBank/Banco/Teste/TesteArrayReferencias.java
+++ br/com/ByteBank/Banco/Teste/TesteArrayReferencias.java
@@ -1,12 +1,29 @@
 package br.com.ByteBank.Banco.Teste;
 
+
+import br.com.ByteBank.Banco.Modelo.Cliente;
 import br.com.ByteBank.Banco.Modelo.ContaCorrente;
+import br.com.ByteBank.Banco.Modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
     public static void main(String[] args) {
         // Nenhuma conta foi criada apenas guarda 5 contas.
-        ContaCorrente[] contas = new ContaCorrente[5];
-       
+        Object[] referencias = new Object[5];
 
+        Cliente c1 = new Cliente();
+        referencias[2] = c1;
+        c1.setNome("Caludia"); 
+
+        System.out.println(c1.getNome());
+       
+        ContaCorrente cc = new ContaCorrente(41, 52);
+        referencias[0] = cc;
+
+        ContaPoupanca cc2 = new ContaPoupanca(45, 78);
+        referencias[1] = cc2;
+
+        ContaPoupanca ref = (ContaPoupanca) referencias[1]; //type cast
+
+        System.out.println(ref.getNumero());
     }
 }
